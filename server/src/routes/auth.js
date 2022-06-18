0ecf54d1d9dfd9c7d6da3b1566425c623ae6c842
mykomiparanum <m.miparanum@gmail.com>
2022-06-18 16:49:51 +0200

    auth boilerplate

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,8 +1,23 @@
 const { Router } = require('express');
+const {
+	getUsers,
+	register,
+	login,
+	protected,
+	logout,
+} = require('../controllers/auth');
+const {
+	validationMiddleware,
+} = require('../middlewares/validations-middleware');
+const { registerValidation, loginValidation } = require('../validators/auth');
+const { userAuth } = require('../middlewares/auth-middleware');
 const router = Router();
 
-router.get('/register', (req, res) => {
-	return res.send('register route Working!');
-});
+router.get('/get-users', userAuth, getUsers);
+router.get('/protected', userAuth, protected);
+router.post('/register', registerValidation, validationMiddleware, register);
+router.post('/login', loginValidation, validationMiddleware, login);
+router.get('/logout', userAuth, logout);
+router.get('/create-user', userAuth, protected);
 
 module.exports = router;
