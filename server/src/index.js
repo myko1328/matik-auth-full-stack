62efd6074b694b77d3f0bfed92b0f94328432cd1
mykomiparanum <m.miparanum@gmail.com>
2022-06-18 16:49:38 +0200

    update middleware

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,18 @@
 const express = require('express');
-const { PORT } = require('./constants');
 const app = express();
+const { PORT, CLIENT_URL } = require('./constants');
+const cookieParser = require('cookie-parser');
+const passport = require('passport');
+const cors = require('cors');
+
+//import passport middleware
+require('./middlewares/passport-middleware');
+
+//initialize middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: CLIENT_URL, credentials: true }));
+app.use(passport.initialize());
 
 //import routes
 const authRoutes = require('./routes/auth');
@@ -8,10 +20,11 @@ const authRoutes = require('./routes/auth');
 //initialize routes
 app.use('/api', authRoutes);
 
+//app start
 const appStart = () => {
 	try {
 		app.listen(PORT, () => {
-			console.log(`The app is running at PORT:${PORT}`);
+			console.log(`The app is running at http://localhost:${PORT}`);
 		});
 	} catch (error) {
 		console.log(`Error: ${error.message}`);
